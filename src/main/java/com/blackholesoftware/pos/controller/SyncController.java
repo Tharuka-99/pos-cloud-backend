447e05ff893e8d34c8d403906fd837da4e71f2ea
Tharuka-99 <tharukanayanajith921@gmail.com>
2026-09-16 01:44:32 +0530

    Fix sync controller base path mapping to /api/sync

--- src/main/java/com/blackholesoftware/pos/controller/SyncController.java
+++ src/main/java/com/blackholesoftware/pos/controller/SyncController.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/sync")
+@RequestMapping("/sync")
 public class SyncController {
 
     // app_users Sync Request එක Receive කරගෙන OK 200 Return කරනවා
